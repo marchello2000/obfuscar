95ac8e30d87b5a02d5f54d3f478f55f430ed8bf2
Lex Li <support@lextm.com>
2018-03-08 12:49:15 -0500

    Fixed the link

---
+++ Core/Helpers/TypeDefinitionExtensions.cs
@@ -0,0 +1,96 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Obfuscar.Helpers
+{
+    internal static class TypeDefinitionExtensions
+    {
+        static public bool IsTypePublic(this TypeDefinition type)
+        {
+            if (type.DeclaringType == null)
+                return type.IsPublic;
+
+            if (type.IsNestedFamily || type.IsNestedFamilyOrAssembly || type.IsNestedPublic)
+                return IsTypePublic(type.DeclaringType);
+
+            return false;
+        }
+
+        public static bool? MarkedToRename(this IMemberDefinition type, bool fromMember = false)
+        {
+#pragma warning disable 618
+            var obfuscarObfuscate = typeof(ObfuscateAttribute).FullName;
+#pragma warning restore 618
+            var reflectionObfuscate = typeof(System.Reflection.ObfuscationAttribute).FullName;
+
+            foreach (CustomAttribute attr in type.CustomAttributes)
+            {
+                var attrFullName = attr.Constructor.DeclaringType.FullName;
+                if (attrFullName == obfuscarObfuscate)
+                    return (bool) (Helper.GetAttributePropertyByName(attr, "ShouldObfuscate") ?? true);
+
+                if (attrFullName == reflectionObfuscate)
+                {
+                    var applyToMembers = (bool) (Helper.GetAttributePropertyByName(attr, "ApplyToMembers") ?? true);
+                    var rename = !(bool) (Helper.GetAttributePropertyByName(attr, "Exclude") ?? true);
+
+                    if (fromMember && !applyToMembers)
+                        return !rename;
+
+                    return rename;
+                }
+            }
+
+            return type.DeclaringType == null ? null : MarkedToRename(type.DeclaringType, true);
+        }
+
+        static IDictionary<string, bool> MemoryCache = new Dictionary<string, bool>();
+
+        public static bool IsResourcesType(this TypeDefinition type)
+        {
+            if (MemoryCache.ContainsKey(type.FullName))
+                return MemoryCache[type.FullName];
+
+            var generated = type.CustomAttributes.FirstOrDefault(attribute =>
+                attribute.AttributeType.FullName == "System.CodeDom.Compiler.GeneratedCodeAttribute");
+            var result = false;
+            if (generated == null)
+            {
+                result = type.IsFormOrUserControl();
+            }
+            else
+            {
+                var name = generated.ConstructorArguments[0].Value.ToString();
+                result = name == "System.Resources.Tools.StronglyTypedResourceBuilder";
+            }
+
+            MemoryCache.Add(type.FullName, result);
+            return result;
+        }
+
+        private static bool IsFormOrUserControl(this TypeDefinition type)
+        {
+            if (type == null)
+                return false;
+
+            if (type.FullName == "System.Windows.Forms.Form" || type.FullName == "System.Windows.Forms.UserControl")
+                return true;
+
+            if (type.BaseType != null)
+            {
+                if (type.BaseType.FullName == "System.Object" &&
+                    type.BaseType.Module.FileName.EndsWith(".winmd", StringComparison.OrdinalIgnoreCase))
+                {
+                    // IMPORTANT: Resolve call below fails for UWP .winmd files.
+                    return false;
+                }
+
+                return type.BaseType.Resolve().IsFormOrUserControl();
+            }
+
+            return false;
+        }
+    }
+}
